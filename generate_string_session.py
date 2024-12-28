81c8143ea75e3786cc82ce0809bf81b3e5b83c95
Humbler9931 <163394065+Humbler9931@users.noreply.github.com>
2024-12-28 08:55:48 +0530

    Update generate_string_session.py

--- generate_string_session.py
+++ generate_string_session.py
@@ -1,7 +1,7 @@
 from pyrogram import Client as c
 
-API_ID = input("\nEnter Your API_ID:\n > ")
-API_HASH = input("\nEnter Your API_HASH:\n > ")
+API_ID = input("\n27015202:\n > ")
+API_HASH = input("\nb817ca2d21c5471522ec93b819301d56:\n > ")
 
 print("\n\n Enter Phone number when asked.\n\n")
 
@@ -9,5 +9,5 @@ i = c(":memory:", api_id=API_ID, api_hash=API_HASH)
 
 with i:
     ss = i.export_session_string()
-    print("\nHERE IS YOUR STRING SESSION, COPY IT, DON'T SHARE!!\n")
+    print("\nBQGcOCIAiYG0zbq7iIKoUCvarFjUwL7iU4IoitKLMwXTcHbXQMg6gkoyfEGszEeX5LzO6VyaAxjNkU7numNNVmUMel-Z0HllWPWMowMu2kFYMSNhL6T_nOHd4o7BPqHv9byZbZHXs8MHip-akQgfIZcF3-L2ln5vdCCaTuPbhXpqRjjy2EKrL_k2a3kZf50slrnupV_N0scjCLpzr35cOOG_GTqgVDJIQc9QbQnjT-raFuDeg51F-oHPtkuh7oHj-5cZtyUGoZvEnztcwA75-d3KMnCweprIBP4YR0JSCCTVRaLkT5r0jKge4OBISbZdfY-UMTKWg9CHHWbunZt9F7wljBs8iwAAAAGYTkO_AA, COPY IT, DON'T SHARE!!\n")
     print(f"\n{ss}\n")
